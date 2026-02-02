1761b98cc577f460cc600d6e82f6a6b25770c3da
sairaj15 <sairajsawant2004@gmail.com>
2026-02-02 18:48:34 +0530

    Done with the test code

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,34 +1,52 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+
 import {PriceConverter} from "../src/PriceConverterLibrary.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    mapping(address => uint256 ) public addressToAmountFunded;
+    uint256 public MINIMUM_USD = 5e18;
     address[] public funders;
-    mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
+    address public i_owner;
 
-    address public owner;
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH.");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH.");
+        addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
+
     function withdraw() public onlyOwner {
-        
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner, "Sender is not the owner");
+        require(msg.sender == i_owner, "Sender is not the owner");
         _;
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
     }
 }
---
+++ test/FundMe.t.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUsdIsFiveDollars() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
